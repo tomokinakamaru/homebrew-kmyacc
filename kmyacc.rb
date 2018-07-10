30d119545d9a0a6d26b30ff2de4c68699a0335a2
TomokiNakamaru <tomoki.nakamaru@gmail.com>
2018-07-10 14:13:14 +0900

    Add kmyacc.rb

---
+++ kmyacc.rb
@@ -0,0 +1,13 @@
+require "formula"
+
+class Kmyacc < Formula
+  homepage "https://github.com/moriyoshi/kmyacc-forked"
+  url "https://github.com/moriyoshi/kmyacc-forked/archive/master.zip"
+  sha256 "31c9635f7922c23957f4e08830f73341436490c4b06139d845f11bde987a2c4e"
+  version "4.1.4"
+
+  def install
+    system "sed -i -e \"s,PREFIX = /usr/local,PREFIX = #{prefix},\" src/Makefile"
+    system "make", "install"
+  end
+end
